b4441852d04de7dac6d75f88f28c5142c97de553
Jai <contactjaitiwari@gmail.com>
2016-02-21 23:09:34 -0500

    Row layout created for RecyclerView

--- app/src/main/java/slidenerd/jait/bucketdrops/ActivityMain.java
+++ app/src/main/java/slidenerd/jait/bucketdrops/ActivityMain.java
@@ -2,11 +2,11 @@ package slidenerd.jait.bucketdrops;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 
@@ -14,6 +14,18 @@ public class ActivityMain extends AppCompatActivity {
 
     Toolbar mToolbar;
     Button mBtnAdd;
+    RecyclerView mRecycler;
+    private View.OnClickListener mBtnAddListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            showDialogAdd();
+        }
+    };
+
+    private void showDialogAdd() {
+        DialogAdd dialog = new DialogAdd();
+        dialog.show(getSupportFragmentManager(), "Add");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,12 +33,8 @@ public class ActivityMain extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
         mBtnAdd = (Button) findViewById(R.id.btn_add);
-        mBtnAdd.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(ActivityMain.this, "Button was clicked!!!", Toast.LENGTH_SHORT).show();
-            }
-        });
+        (mRecycler) = (RecyclerView) findViewById(R.id.rv_drops);
+        mBtnAdd.setOnClickListener(mBtnAddListener);
         setSupportActionBar(mToolbar);
         initBackgroundImage();
     }
